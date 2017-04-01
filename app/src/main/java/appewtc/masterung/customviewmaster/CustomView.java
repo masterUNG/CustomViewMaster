c1b0537f5020ddcc5ca595d48cd10c616550ad54
masterUNG <phrombutr@gmail.com>
2017-04-01 13:30:21 +0700

    Create attrs.xml

--- app/src/main/java/appewtc/masterung/customviewmaster/CustomView.java
+++ app/src/main/java/appewtc/masterung/customviewmaster/CustomView.java
@@ -1,6 +1,9 @@
 package appewtc.masterung.customviewmaster;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -12,17 +15,34 @@ import android.view.View;
 public class CustomView extends View{
     public CustomView(Context context) {
         super(context);
+        init();
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), paint);
+
     }
 }   // Main Class
