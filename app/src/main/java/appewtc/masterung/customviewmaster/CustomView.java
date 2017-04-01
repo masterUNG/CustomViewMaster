da39b4f9434ac9d01fb160d21de83c8f11ddfe36
masterUNG <phrombutr@gmail.com>
2017-04-01 12:22:51 +0700

    Create CustomView Class

---
+++ app/src/main/java/appewtc/masterung/customviewmaster/CustomView.java
@@ -0,0 +1,28 @@
+package appewtc.masterung.customviewmaster;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by masterUNG on 4/1/2017 AD.
+ */
+
+public class CustomView extends View{
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}   // Main Class
